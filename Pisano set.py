91816f11da3f595225a65a3bdbd3d1ed7b8fa19a
Jason Datuin Andoyo <jasonandoyo8000@gmail.com>
2026-04-09 21:01:01 +0800

    Update Pisano set.py

--- Pisano set.py
+++ Pisano set.py
@@ -4,19 +4,22 @@
 from sympy import legendre_symbol, primerange
 
 def check(p,in1,in2,k):
-
     L, a, b, in1, in2 = [], in1, in2, in1%p, in2%p
-    
     while True:
-
         L.append(legendre_symbol(a, p))
-
         a, b = b, (a + b)%p
-
         if (a, b) == (in1, in2): break
-
     return L.count(1) - L.count(-1) - L.count(0) == k 
-a, b, k, n = 0, 1, 0, 3000
 
-kpisano = [p for p in primerange(3, n) if check(p,a,b,k)]
-print(kpisano)
+def pisanoset(in1,in2,k1,k2,maxp):
+    prime = list(primerange(3,maxp))
+    kint = range(k1,k2+1,1)
+    pisanoset = []
+    for k in kint:
+        for p in prime:
+            if check(p,in1,in2,k) == True: 
+                pisanoset.append(k)
+                break
+    return pisanoset
+    
+print(pisanoset(0,1,-100,100,1000))
